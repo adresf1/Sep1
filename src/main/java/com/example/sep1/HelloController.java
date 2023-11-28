1ac1bda4268607e18ef5d593331d87b1db63a7cb
adresf1 <147700879+adresf1@users.noreply.github.com>
2023-11-28 15:35:55 +0100

    .

--- src/main/java/com/example/sep1/HelloController.java
+++ src/main/java/com/example/sep1/HelloController.java
@@ -7,6 +7,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 import static javafx.scene.paint.Color.rgb;
 
@@ -17,23 +20,21 @@ public class HelloController
     @FXML private PasswordField Password;
     @FXML private Button cancel;
     @FXML private Label resp;
+    private Oversigt oversigt;
 
 
     private LoginModel model1 = new LoginModel();
     private Paint rgb;
 
-    public void OnButtonPressed()
-    {
+    public void OnButtonPressed() throws IOException {
         String input = username.getText();
         String pass = Password.getText();
-        if (model1.login(input, pass))
-        {
-            resp.setText("You have logged in seccesfully");
+        if (model1.login(input, pass)) {
+
+            resp.setText("You have logged in successfully");
             resp.setTextFill(rgb(0, 255, 0));
-            Oversigt frontPage = new Oversigt();
-        }
-        else
-        {
+
+        } else {
             resp.setText("Wrong, try again");
             resp.setTextFill(rgb(255, 0, 0));
         }
--- src/main/java/com/example/sep1/Oversigt.java
+++ src/main/java/com/example/sep1/Oversigt.java
@@ -1,20 +1,27 @@
 package com.example.sep1;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Oversigt {
-
+public class Oversigt extends Application {
+    @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("oversigt.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Oversigt.class.getResource("Oversigt.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         HelloController helloController = fxmlLoader.getController();
         LoginModel login = new LoginModel();
-        stage.setTitle("Hello!");
+        stage.setTitle("Oversigt!");
+
         stage.setScene(scene);
+
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
