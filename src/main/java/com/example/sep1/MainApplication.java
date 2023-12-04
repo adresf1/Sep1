6a280db539064651c055dbaf021764a22fe63946
adresf1 <147700879+adresf1@users.noreply.github.com>
2023-12-04 23:25:38 +0100

    .

--- src/main/java/com/example/sep1/MainApplication.java
+++ src/main/java/com/example/sep1/MainApplication.java
@@ -30,16 +30,21 @@ public class MainApplication extends Application {
         FileInputStream fileInputStream3 = new FileInputStream("dataindustri.bin");
         ObjectInputStream objectInputStream3 = new ObjectInputStream(fileInputStream3);
 
+        /*
         Boligbyggerilist boligbyggerilist = (Boligbyggerilist) objectInputStream.readObject();
         Vejbygninglist vejbygninglist = (Vejbygninglist) objectInputStream2.readObject();
         Kommerciellelist kommerciellelist=(Kommerciellelist) objectInputStream1.readObject();
         Industrillelist industrillelist=(Industrillelist) objectInputStream3.readObject();
 
 
-        controller.init( boligbyggerilist);
-        controller.init( vejbygninglist);
-        controller.init( kommerciellelist);
-        controller.init( industrillelist);
+         */
+
+
+
+        controller.init(new Boligbyggerilist() );
+        controller.init(new Vejbygninglist() );
+        controller.init( new Kommerciellelist());
+        controller.init(new Industrillelist() );
 
 
 
